45bcce1d6b6435a17eee07c942e8cb89bbddf428
Redtri <red.tri@outlook.com>
2018-10-11 11:28:01 +0200

    Adding : Variables declaration, stream operations

--- CPP_Intro/main.cpp
+++ CPP_Intro/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string>
+
 
 int main()
 {
+	int nInteger = 0;
+	double dblDouble = 0.0f;
+	char cChar = 'a';
+	bool blnBoolean = 1;
+
+	std::string strString = "Bob";
+
+	//Output
 	std::cout << "Hellow world!" << std::endl;
+
+	//Input
+	std::cin >> nInteger;
+
+	//Output
+	std::cout << "Valeur de l'entier : " << nInteger << std::endl;
+	std::cout << strString << std::endl;
+
+	std::cout << "Entrez votre prenom" << std::endl;
+	std::getline(std::cin, strString);
+	std::cout << "Prenom : " << strString << std::endl;
+
+	system("pause");
+
+	return 0;
 }
